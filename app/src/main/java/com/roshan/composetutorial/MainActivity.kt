2fe67d15a730d023e641f7e0d31e35ca279e51b6
Roshan Rai <rrai@mast.com>
2021-11-07 11:58:53 +0530

    Text styling and fonts

--- app/src/main/java/com/roshan/composetutorial/MainActivity.kt
+++ app/src/main/java/com/roshan/composetutorial/MainActivity.kt
@@ -3,112 +3,69 @@ package com.roshan.composetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.sp
-import com.roshan.composetutorial.ui.theme.ComposeTutorialTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val painter = painterResource(id = R.drawable.kermit)
-            val contentDescription = "This is Kermit Image"
-            val title = "Kermit is playing in the snow"
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth(0.5f)
-                    .padding(16.dp)
-            ) {
-                ImageCard(painter = painter, contentDescription = contentDescription, title = title)
-            }
-
-        }
-    }
-}
-
-@Composable
-fun ImageCard(
-    painter: Painter,
-    contentDescription: String,
-    title: String,
-    modifier: Modifier = Modifier
-) {
-    Card(
-        modifier = modifier.fillMaxWidth(),
-        shape = RoundedCornerShape(15.dp),
-        elevation = 5.dp
-    ) {
-        Box(modifier = Modifier.height(200.dp)) {
-            Image(
-                painter = painter,
-                contentDescription = contentDescription,
-                contentScale = ContentScale.Crop
-            )
+            val fontFamily =
+                FontFamily(
+                    Font(R.font.lexend_thin, FontWeight.Thin),
+                    Font(R.font.lexend_light, FontWeight.Light),
+                    Font(R.font.lexend_regular, FontWeight.Normal),
+                    Font(R.font.lexend_medium, FontWeight.Medium),
+                    Font(R.font.lexend_semibold, FontWeight.SemiBold),
+                    Font(R.font.lexend_bold, FontWeight.Bold),
+                    Font(R.font.lexend_extrabold, FontWeight.ExtraBold)
+                )
             Box(
                 modifier = Modifier
                     .fillMaxSize()
-                    .background(
-                        Brush.verticalGradient(
-                            colors = listOf(
-                                Color.Transparent, Color.Black
-                            ), startY = 300f
-                        )
-                    )
-            )
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(12.dp), contentAlignment = Alignment.BottomStart
+                    .background(Color.Black)
             ) {
-                Text(title, style = TextStyle(color = Color.White, fontSize = 16.sp))
+                Text(
+                    text = buildAnnotatedString {
+                        withStyle(
+                            style = SpanStyle(color = Color.Green, fontSize = 50.sp)
+                        ) {
+                            append("J")
+                        }
+                        append("etpack")
+                        withStyle(
+                            style = SpanStyle(color = Color.Green, fontSize = 50.sp)
+                        ) {
+                            append("C")
+                        }
+                        append("ompose")
+
+                    },
+                    color = Color.White,
+                    fontSize = 30.sp,
+                    fontFamily = fontFamily,
+                    fontWeight = FontWeight.Bold,
+                    fontStyle = FontStyle.Italic,
+                    textAlign = TextAlign.Center,
+                    textDecoration = TextDecoration.Underline
+
+                )
             }
+
         }
-    }
-
-}
-
-@Composable
-fun Greeting(name: String) {
-    Column(
-        Modifier
-            .background(Color.Green)
-            .fillMaxHeight(.5f)
-            .fillMaxWidth()
-            .border(5.dp, Color.Magenta)
-            .padding(5.dp)
-            .border(5.dp, Color.Blue)
-
-//        horizontalAlignment = Alignment.CenterHorizontally,
-//        verticalArrangement = Arrangement.SpaceAround
-    ) {
-        Text(text = "Hello1 $name!")
-        Spacer(modifier = Modifier.height(50.dp))
-        Text(text = "Hello2 $name!")
-    }
-
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeTutorialTheme {
-        Greeting("Android")
     }
 }
