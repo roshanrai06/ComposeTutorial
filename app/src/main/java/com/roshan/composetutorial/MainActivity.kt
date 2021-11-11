524f04c172741f4f6e2facd3f4d78c026a3e8db2
Roshan Rai <rrai@mast.com>
2021-11-11 08:23:18 +0530

    Side effects and dispose effects

--- app/src/main/java/com/roshan/composetutorial/MainActivity.kt
+++ app/src/main/java/com/roshan/composetutorial/MainActivity.kt
@@ -2,34 +2,61 @@ package com.roshan.composetutorial
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.OnBackPressedDispatcher
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Button
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.*
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LazyColumn() {
-                items(5000){
-                    Text(
-                        text = "Item $it",
-                        fontSize = 24.sp,
-                        fontWeight = FontWeight.Bold,
-                        textAlign = TextAlign.Center,
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(vertical = 24.dp)
-                    )
+            val scope = rememberCoroutineScope()
+            val scaffoldState = rememberScaffoldState()
+            Scaffold(scaffoldState = scaffoldState) {
+//                var counter by remember {
+//                    mutableStateOf(0)
+//                }
+                val counter = produceState(initialValue = 0) {
+                    delay(300)
+                    value = 4
+                }
+                if (counter.value % 5 == 0 && counter.value > 0) {
+                    LaunchedEffect(key1 = scaffoldState) {
+                        scaffoldState.snackbarHostState.showSnackbar(
+                            "Hello"
+                        )
+                    }
+
+                }
+                Button(onClick = { }) {
+                    Text(text = "Click Me : $counter")
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun DisposalCompose(backPressedDispatcher: OnBackPressedDispatcher) {
+    val callback = remember {
+        object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                // Do Something
+            }
+
+        }
+    }
+
+    DisposableEffect(key1 = backPressedDispatcher) {
+        backPressedDispatcher.addCallback(callback)
+        onDispose {
+            callback.remove()
         }
     }
 }
